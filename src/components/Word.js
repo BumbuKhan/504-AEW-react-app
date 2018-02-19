7d2a78b3b47b178e9307c9f149f287288a172240
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-02-19 23:57:50 +0400

    Added bootstrap card style to Word component

--- src/components/Word.js
+++ src/components/Word.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default (props) => {
     return (
-        <div>A single word</div>
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">abandon</h5>
+                <h6 className="card-subtitle mb-3 text-muted">desert, leave without planning to come back, quit</h6>
+                <p className="card-text word__example">When Roy *abandoned* his family, the police went looking for him.</p>
+                <p className="card-text word__example">The soldier could not *abandon* his friends who were hurt in battle.</p>
+                <p className="card-text word__example">Because Rose was poor, she had to *abandon* her idea of going to college.</p>
+            </div>
+        </div>
     );
 };
