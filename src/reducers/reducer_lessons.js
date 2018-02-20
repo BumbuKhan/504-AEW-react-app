59ee17c403e229cf1bc02f95275cddc30a993111
Курбан Курбанов <qurban.qurbanov93@gmail.com>
2018-02-20 10:54:20 +0400

    now lessons list are correctly installed

--- src/reducers/reducer_lessons.js
+++ src/reducers/reducer_lessons.js
@@ -4,8 +4,7 @@ const defaultState = [];
 export default (state = defaultState, action) => {
     switch (action.type) {
         case FETCH_LESSONS:
-            console.log(action.payload.data.items);
-            return {lessons: action.payload.data.items};
+            return [...action.payload.data.items];
         default:
             console.log('001');
             return state;
