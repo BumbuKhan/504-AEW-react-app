5b55995b622191db211bc0ede74c0959ba8a6d3e
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-02-20 07:53:54 +0400

    a dummy component Lesson has been added

---
+++ src/components/Lesson.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <div className="list-group">
+                <a href="" className="list-group-item list-group-item-action">Lesson 1
+                    <span className="badge badge-primary badge-pill lessons-list__badge">12</span>
+                </a>
+                <a href="" className="list-group-item list-group-item-action">Lesson 2
+                    <span className="badge badge-primary badge-pill lessons-list__badge">12</span>
+                </a>
+                <a href="" className="list-group-item list-group-item-action">Lesson 3
+                    <span className="badge badge-primary badge-pill lessons-list__badge">12</span>
+                </a>
+            </div>
+        </div>
+    )
+}
--- src/containers/LessonsList.js
+++ src/containers/LessonsList.js
@@ -1,22 +1,11 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Lesson from '../components/Lesson';
 
 class LessonsList extends Component {
     render() {
         return (
-            <div>
-                <div className="list-group">
-                    <a href="" className="list-group-item list-group-item-action">Lesson 1
-                        <span className="badge badge-primary badge-pill lessons-list__badge">12</span>
-                    </a>
-                    <a href="" className="list-group-item list-group-item-action">Lesson 2
-                        <span className="badge badge-primary badge-pill lessons-list__badge">12</span>
-                    </a>
-                    <a href="" className="list-group-item list-group-item-action">Lesson 3
-                        <span className="badge badge-primary badge-pill lessons-list__badge">12</span>
-                    </a>
-                </div>
-            </div>
+            <Lesson />
         );
     }
 }
